246fdd81d08b6ab2e02763462abfea5cc0dca054
谢万超 <15704912683@163.com>
2025-01-22 10:14:49 +0800

    网络配置

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,4 @@
-export const serverIp = "124.222.217.145"
+// export const serverIp = "124.222.217.145"
+export const serverIp = "localhost"
 // export const serverIp = "49.235.115.169"
 export const serverPort = "5000"
